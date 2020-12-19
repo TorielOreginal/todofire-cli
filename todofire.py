51e829dfd5cf1de5a946f038f9d4c006bf0e7e9f
Toriel Oreginau <72525388+TorielOreginal@users.noreply.github.com>
2020-12-19 14:30:15 -0300

    Add files via upload

---
+++ todofire.py
@@ -0,0 +1,38 @@
+import sqlite3
+import sys
+
+db = sqlite3.connect('todo.db')
+c = db.cursor()
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS todos(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR,
+        done BOOLEAN
+    )
+''')
+
+while True:
+    opr = input('>> ')
+
+    if opr == 'show':
+        c.execute('SELECT * FROM todos')
+        print(c.fetchall())
+    if opr == 'new':
+        name = input('Input the todo name: ')
+        c.execute('''
+            INSERT INTO todos (name, done) VALUES(
+                ?,
+                ?
+            )
+        ''', (name, 'FALSE'))
+    if opr == 'delete':
+        id = input('Input the todo id: ')
+        c.execute('DELETE FROM todos WHERE id=?', id)
+    if opr == 'done':
+        id = input('Input the todo id: ')
+        c.execute('UPDATE todos SET done=? WHERE id=?', ('TRUE', id))
+    if opr == 'exit':
+        db.commit()
+        db.close()
+        sys.exit(0)
